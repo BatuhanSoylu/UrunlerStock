d01194cb4fce5126a7645025d72ee7daa5122143
BatuhanSoylu <batuhansoylueee@gmail.com>
2023-09-25 15:50:10 +0300

    Add project files.

---
+++ RecapProject1/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace RecapProject1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxCategories = new System.Windows.Forms.GroupBox();
+            this.gbxProduct = new System.Windows.Forms.GroupBox();
+            this.dgwProduct = new System.Windows.Forms.DataGridView();
+            this.lblCategories = new System.Windows.Forms.Label();
+            this.lblProduct = new System.Windows.Forms.Label();
+            this.txtProduct = new System.Windows.Forms.TextBox();
+            this.cmbbxCategories = new System.Windows.Forms.ComboBox();
+            this.gbxCategories.SuspendLayout();
+            this.gbxProduct.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbxCategories
+            // 
+            this.gbxCategories.Controls.Add(this.cmbbxCategories);
+            this.gbxCategories.Controls.Add(this.lblCategories);
+            this.gbxCategories.Location = new System.Drawing.Point(33, 33);
+            this.gbxCategories.Name = "gbxCategories";
+            this.gbxCategories.Size = new System.Drawing.Size(1069, 100);
+            this.gbxCategories.TabIndex = 0;
+            this.gbxCategories.TabStop = false;
+            this.gbxCategories.Text = "Kategoriye Göre Ara";
+            // 
+            // gbxProduct
+            // 
+            this.gbxProduct.Controls.Add(this.txtProduct);
+            this.gbxProduct.Controls.Add(this.lblProduct);
+            this.gbxProduct.Location = new System.Drawing.Point(33, 152);
+            this.gbxProduct.Name = "gbxProduct";
+            this.gbxProduct.Size = new System.Drawing.Size(1069, 100);
+            this.gbxProduct.TabIndex = 1;
+            this.gbxProduct.TabStop = false;
+            this.gbxProduct.Text = "Ürün İsmine Göre Ara";
+            // 
+            // dgwProduct
+            // 
+            this.dgwProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwProduct.Location = new System.Drawing.Point(33, 288);
+            this.dgwProduct.Name = "dgwProduct";
+            this.dgwProduct.RowHeadersWidth = 51;
+            this.dgwProduct.RowTemplate.Height = 24;
+            this.dgwProduct.Size = new System.Drawing.Size(1069, 256);
+            this.dgwProduct.TabIndex = 2;
+            // 
+            // lblCategories
+            // 
+            this.lblCategories.AutoSize = true;
+            this.lblCategories.Location = new System.Drawing.Point(20, 28);
+            this.lblCategories.Name = "lblCategories";
+            this.lblCategories.Size = new System.Drawing.Size(57, 16);
+            this.lblCategories.TabIndex = 0;
+            this.lblCategories.Text = "Kategori";
+            // 
+            // lblProduct
+            // 
+            this.lblProduct.AutoSize = true;
+            this.lblProduct.Location = new System.Drawing.Point(20, 34);
+            this.lblProduct.Name = "lblProduct";
+            this.lblProduct.Size = new System.Drawing.Size(35, 16);
+            this.lblProduct.TabIndex = 0;
+            this.lblProduct.Text = "Ürün";
+            // 
+            // txtProduct
+            // 
+            this.txtProduct.Location = new System.Drawing.Point(126, 34);
+            this.txtProduct.Name = "txtProduct";
+            this.txtProduct.Size = new System.Drawing.Size(215, 22);
+            this.txtProduct.TabIndex = 1;
+            this.txtProduct.TextChanged += new System.EventHandler(this.txtProduct_TextChanged);
+            // 
+            // cmbbxCategories
+            // 
+            this.cmbbxCategories.FormattingEnabled = true;
+            this.cmbbxCategories.Location = new System.Drawing.Point(126, 28);
+            this.cmbbxCategories.Name = "cmbbxCategories";
+            this.cmbbxCategories.Size = new System.Drawing.Size(215, 24);
+            this.cmbbxCategories.TabIndex = 1;
+            this.cmbbxCategories.SelectedIndexChanged += new System.EventHandler(this.cmbbxCategories_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1156, 556);
+            this.Controls.Add(this.dgwProduct);
+            this.Controls.Add(this.gbxProduct);
+            this.Controls.Add(this.gbxCategories);
+            this.Name = "Form1";
+            this.Text = "Ürünler";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbxCategories.ResumeLayout(false);
+            this.gbxCategories.PerformLayout();
+            this.gbxProduct.ResumeLayout(false);
+            this.gbxProduct.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwProduct)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxCategories;
+        private System.Windows.Forms.ComboBox cmbbxCategories;
+        private System.Windows.Forms.Label lblCategories;
+        private System.Windows.Forms.GroupBox gbxProduct;
+        private System.Windows.Forms.TextBox txtProduct;
+        private System.Windows.Forms.Label lblProduct;
+        private System.Windows.Forms.DataGridView dgwProduct;
+    }
+}
+
---
+++ RecapProject1/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecapProject1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadProducts();
+            LoadCategories();
+        }
+
+        private void LoadProducts()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                dgwProduct.DataSource = context.Products.ToList();
+            }
+        }
+        private void LoadProductsByCategory(int Id)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                dgwProduct.DataSource = context.Products.Where(p=> p.CategoryId==Id).ToList();
+            }
+        }
+        private void LoadProductsByName(string name)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                dgwProduct.DataSource = context.Products.Where(p => p.ProductName.ToLower().Contains(name.ToLower())).ToList();
+            }
+        }
+        private void LoadCategories()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                cmbbxCategories.DataSource= context.Categories.ToList();
+                cmbbxCategories.DisplayMember= "CategoryName";
+                cmbbxCategories.ValueMember = "CategoryId";
+            }
+        }
+
+        private void cmbbxCategories_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                LoadProductsByCategory(Convert.ToInt32(cmbbxCategories.SelectedValue));
+            }
+            catch 
+            {
+            }
+
+
+        }
+
+        private void txtProduct_TextChanged(object sender, EventArgs e)
+        {
+            string key = txtProduct.Text;
+            if (string.IsNullOrEmpty(key))
+            {
+                LoadProducts(); 
+            }
+            else
+            {
+                LoadProductsByName(key);
+
+            }
+        }
+    }
+}
